9db32a915ee8f8254ddf5802dfdcc831e9a8f493
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-10-28 11:31:37 +0600

    pizza

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
 </head>
 
 <body>
-    <form action="index.php" method="get">
+
+
+    <!-- <form action="index.php" method="get"> -->
+
+        <!-- <form action="index.php" method="post">
 
         <h2>First day php </h2>
         <label>username:</label>
@@ -17,19 +21,56 @@
         <input type="Password" name="Password"><br><br>
         <input type="submit" value="Log in"><br><br>
 
-    </form>
+    </form> -->
+
+<!-- order PIZZA -->
+ <form action="index.php" method="post">
+
+    <label>quantity:</label>
+    <input type="text" name="quantity">
+    <input type="submit" value="total">
+
+
+
+ </form>
+
 
     <?php
-    //isset = key check
+
+
+    //isset = key check/ variable / array key
     //$_GET = key = input name, value (same as a user)
 
- if (isset($_GET["username"]) && isset($_GET["Password"])) {
-    // echo $_GET["username"]."<br>";
-    // echo $_GET["Password"]."<br>";
+//  if (isset($_GET["username"]) && isset($_GET["Password"])) {
+//     // echo $_GET["username"]."<br>";
+//     // echo $_GET["Password"]."<br>";
 
-    echo "{$_GET["username"]} <br>";
-    echo "{$_GET["Password"]} <br>";
-}
+//     // echo "{$_GET["username"]} <br>";
+//     // echo "{$_GET["Password"]} <br>";
+
+    
+// } 
+
+// if (isset($_POST["username"]) && isset($_POST["Password"])){
+//     echo "{$_POST["username"]} <br>";
+//      echo "{$_POST["Password"]} <br>";
+// }  
+
+
+//oder pizza
+$items= "pizza";
+$price = 5.99;
+ $quantity = $_POST["quantity"];
+ $total= null;
+ $total = $quantity * $price ;
+
+
+ echo"you have ordered {$quantity} x {$items}/s <br>" ;
+ echo "you total is: \${$total}";
+
+
+
+
     ?>
 </body>
 
